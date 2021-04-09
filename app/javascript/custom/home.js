794cd926fcbb6d8c750daafadf67c351f0dc1215
guillenjs <jorgeguilln@gmail.com>
2021-04-09 16:33:25 -0400

    update to style and formatting

---
+++ app/javascript/custom/home.js
@@ -0,0 +1,5 @@
+// let button = document.querySelector('.extra-button')
+
+// button.addEventListener('click', (evt) => {
+//     console.log('clicking')
+// })
--- app/views/posts/show.json.jbuilder
+++ app/views/posts/show.json.jbuilder
@@ -1,1 +1,5 @@
 json.partial! "posts/post", post: @post
+
+let button = document.querySelector('.extra-button')
+
+console.log(button)
