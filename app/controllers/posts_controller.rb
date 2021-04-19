260b6f2c830ed164d008068858ffc05d30119552
guillenjs <jorgeguilln@gmail.com>
2021-04-19 12:19:21 -0400

    added bootstrap, modal, and path to upload section

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -57,6 +57,9 @@ class PostsController < ApplicationController
     end
   end
 
+  def upload
+  
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
